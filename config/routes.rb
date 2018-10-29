c1aed6d9f9545ecb66c1dbfb347655dda27e0285
behrami <behram1993@gmail.com>
2018-10-29 12:57:10 -0400

    added article and user controller, added routes and started working on signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root 'articles#index'
+
+  resources :users, only:  %i[new create]
+  resources :articles, only: %i[index show]
+  resource :session, only: %i[new create destroy] do
+  	resources :articles, only: %i[new create edit update destroy]
+  end
 end
