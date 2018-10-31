adeeefade5132e51a96999a238bfe2babee43689
behrami <behram1993@gmail.com>
2018-10-31 14:13:31 -0400

    sign in,out and up works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= User.find_by(id:session[:user_id]) if session[:user_id]
+  end
+
+  def ensure_logged_in
+    unless current_user
+      flash[:alert] = "Please log in"
+      redirect_to new_session_path
+    end
+  end
+  
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,35 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(user_name: params[:session][:user_name])
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      # if request.xhr?
+      #   render json: {sign_in: 'Passed Sign In'}
+      # else
+        redirect_to root_path
+      # end
+
+    else
+      flash.now[:alert] = 'Unsuccessful Sign In'
+      # if request.xhr?
+      #   render json: {failed: 'Unsuccessful Sign In'}
+      # else
+        render :new
+      # end
+
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Successfully Signed Out'
+    redirect_to root_path
+  end
+
+end
